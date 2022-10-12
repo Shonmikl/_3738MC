ddf4eb34b88331fdbe79f3d89d69c3b99154109c
shonmikl <mikhailegorov676@gmail.com>
2022-10-12 14:11:23 +0300

    ArrayEx

---
+++ src/Testqq.java
@@ -0,0 +1,17 @@
+public class Testqq {
+    int num1;
+    int num2;
+    String operation;
+
+    double getResult(int num1, int num2, String operation){
+        if("-".equals(operation)) {
+            return num1 *num2;
+        }//итд
+        return 0;
+    }
+
+    void calc() {
+        //тут потом будем писать циклы
+        //можете пока что начать как понимаете
+    }
+}
