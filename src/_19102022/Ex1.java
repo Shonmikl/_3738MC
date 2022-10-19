4b1a1d8e951d19613bf0e907650761556e59800e
shonmikl <mikhailegorov676@gmail.com>
2022-10-19 14:00:22 +0300

    DoWhileEx.java SwitchCaseEx.java TernOp.java

---
+++ src/_19102022/Ex1.java
@@ -0,0 +1,36 @@
+package _19102022;
+
+import java.util.Arrays;
+
+public class Ex1 {
+    public boolean get() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[11];// 0
+        Integer[] arrayL = new Integer[22];// null
+        System.out.println(Arrays.toString(arrayL));
+
+        String[] stringArray = {"1", "2", "3", "4"};
+        int[] array2 = {65, 465, 4, 65, 4};
+
+        System.out.println(array2.length);
+
+        for (int i = 0; i < array.length; i++) {
+
+        }
+
+        for (int a:array2) {
+
+        }
+
+        if(array2.length == 5) {
+
+        }
+
+        while (true) {
+
+        }
+    }
+}
